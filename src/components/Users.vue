2bc652cc1858b1392dad56392bbe2ebbc7cde3c9
kristian-wittrup <kw@easv.dk>
2022-03-01 08:53:02 +0100

    first commit

---
+++ src/components/Users.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p v-for="user in state.users" :key="user.id">
+      {{user.first_name}}
+    </p>
+  </div>
+</template>
+
+<script>
+import { reactive, onMounted } from 'vue'
+
+
+export default {
+  async setup () {
+
+    const state = reactive({
+      users: null,
+      error: null
+    })
+
+    const load = async() => {
+      try {
+        const usersResponse = await fetch("https://reqres.in/api/users")
+        await usersResponse.json().then(userData => {
+          state.users = userData.data
+        })
+      }
+
+      catch(err) {
+        state.error = err.message
+        console.log(err)
+      }
+}
+//await load();
+
+ onMounted(() => {
+  load()
+})
+
+    return {
+      state
+    }
+  }
+ 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
